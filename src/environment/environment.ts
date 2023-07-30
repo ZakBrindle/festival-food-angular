cdc01f0d393ae7aeadf7f10b27fd19c3c4e6c8c8
ZakBrindle <z4kbrindle@gmail.com>
2023-07-30 15:01:02 +0100

    fake environment.js

---
+++ src/environment/environment.ts
@@ -0,0 +1,13 @@
+export const environment ={
+    production:false,
+    firebase: {
+         apiKey: "",
+         authDomain: "festival-food-db.firebaseapp.com",
+         projectId: "festival-food-db",
+         storageBucket: "festival-food-db.appspot.com",
+         messagingSenderId: "",
+         appId: "",
+         measurementId: "G-Y1KG8W3CKP"
+      }
+}
+
