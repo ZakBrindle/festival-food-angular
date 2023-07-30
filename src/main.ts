ab19c10fd22036249d7268b48c8d30635ed03629
ZakBrindle <z4kbrindle@gmail.com>
2023-07-30 15:12:19 +0100

    fake environment.js declared in main.ts fix

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { environment } from './environment/environment';
+
 import { AppModule } from './app/app.module';
 
 
